5a5895320ed0368b9f87f76fed61ef21dc91cbcf
Johnny Guo <johnnyguo2007@gmail.com>
2024-03-12 00:01:52 -0400

    applying hw mask on large xarray.Dataset

--- hw_global/read_zarr.py
+++ hw_global/read_zarr.py
@@ -21,8 +21,6 @@ def append_to_zarr(ds, zarr_group):
 
 def main():
     # Initialize a Dask client to use multiple CPUs
-    client = Client()
-    print("Dask Dashboard:", client.dashboard_link)
 
     # Read the Zarr dataset using Xarray with automatic chunking for Dask
     ds = xr.open_zarr('/home/jguo/process_data/zarr/test02/3Dvars') #, chunks='auto')
@@ -34,7 +32,7 @@ def main():
     ds['UWBI'] = ds['WBA_U'] - ds['WBA_R']
 
     # Read in the daily grid hw netcdf file
-    ds_daily_grid_hw = xr.open_dataset('/home/jguo/process_data/i.e215.I2000Clm50SpGs.hw_production.02/summary/daily_grid_hw.nc', chunks='auto')
+    ds_daily_grid_hw = xr.open_dataset('/home/jguo/process_data/i.e215.I2000Clm50SpGs.hw_production.02/summary/daily_grid_hw.nc')
 
     ds_daily_grid_hw = convert_time(ds_daily_grid_hw)
     ds = convert_time(ds)
@@ -47,8 +45,8 @@ def main():
         print(f"Processing data for year: {year}")
 
         # Filter the dataset for the current year
-        ds_year = ds.sel(time=ds['time'].dt.year == year)
-        ds_daily_grid_hw_year = ds_daily_grid_hw.sel(time=ds_daily_grid_hw['time'].dt.year == year)
+        ds_year = ds.sel(time=ds['time'].dt.year == year).compute()
+        ds_daily_grid_hw_year = ds_daily_grid_hw.sel(time=ds_daily_grid_hw['time'].dt.year == year).compute()
 
         # Example processing for each year
         # Note: Adjust the processing logic below as per your requirements
@@ -61,12 +59,21 @@ def main():
         hourly_mask_year = overlapping_mask_year.reindex(time=full_hourly_range_year, method='ffill').compute()
 
         # Apply the hourly mask to ds_year to get filtered data for the current year
-        filtered_ds_year = ds_year.where(hourly_mask_year, drop=True)
+        filtered_ds_year = ds_year.where(hourly_mask_year)
 
         # Compute and save filtered datasets for the current year
         filtered_ds_compute_year = filtered_ds_year.compute()
         append_to_zarr(filtered_ds_compute_year, f'/home/jguo/process_data/zarr/test02/filtered_ds')
         del filtered_ds_compute_year
+
+        # Invert the hourly mask to target hours where the condition doesn't hold for the current year
+        inverted_hourly_mask_year = (~hourly_mask_year).compute()
+
+        # Apply the inverted mask to ds_year to get unmasked data for the current year
+        unmasked_ds_year = ds_year.where(inverted_hourly_mask_year)
+        unmasked_ds_compute_year = unmasked_ds_year.compute()
+        append_to_zarr(unmasked_ds_compute_year, f'/home/jguo/process_data/zarr/test02/unmasked_ds_year')
+        del unmasked_ds_compute_year
 
 if __name__ == "__main__":
     main()
