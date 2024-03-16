568c357a45d5fdc19ccb933b9e1c5d98156dc5b3
Johnny Guo <johnnyguo2007@gmail.com>
2024-03-16 16:47:08 -0400

    get results

--- hw_global/utils/netcdf_to_zarr.py
+++ hw_global/utils/netcdf_to_zarr.py
@@ -6,6 +6,10 @@ import xarray as xr
 import pandas as pd
 import zarr
 
+def convert_time(ds):
+    """Converts time values in the dataset to pandas Timestamps."""
+    ds['time'] = [pd.Timestamp(time.strftime()) for time in ds['time'].values]
+    return ds
 
 def round_to_nearest_hour(time_values):
     # Function to round cftime.DatetimeNoLeap objects to the nearest hour
@@ -67,7 +71,7 @@ def append_to_zarr(ds, zarr_group):
         ds.to_zarr(zarr_group, mode='w', encoding=encoding, consolidated=True)
 
 
-def process_year(netcdf_dir, zarr_path, log_file_path, year, var_list):
+def process_year(netcdf_dir, zarr_path, log_file_path, year, var_list, ds_daily_grid_hw):
     file_pattern = os.path.join(netcdf_dir, f'i.e215.I2000Clm50SpGs.hw_production.02.clm2.h2.{year}-*-00000.nc')
     file_paths = sorted(glob.glob(file_pattern))
 
@@ -77,42 +81,50 @@ def process_year(netcdf_dir, zarr_path, log_file_path, year, var_list):
         log_file_status(log_file_path, f'No files found for {file_pattern}', "Missing")
         return
 
-    ds = xr.open_mfdataset(file_paths, chunks={'time': 24 * 31})[var_list]
+    ds_year = xr.open_mfdataset(file_paths, chunks={'time': 24 * 31})[var_list]
+    ds_daily_grid_hw_year = ds_daily_grid_hw.sel(time=ds_daily_grid_hw['time'].dt.year == year).compute()
 
     # Store data types before modification
-    data_types_before = {var: ds[var].dtype for var in ds.data_vars}
+    data_types_before = {var: ds_year[var].dtype for var in ds_year.data_vars}
     print("Data types before set_unwanted_to_nan:")
     for var, dtype in data_types_before.items():
         print(f'{var}: {dtype}')
 
-    ds['time'] = round_to_nearest_hour(ds['time'].values)
-    ds = set_unwanted_to_nan(ds)
+    ds_year['time'] = round_to_nearest_hour(ds_year['time'].values)
+    ds_year = convert_time(ds_year)
+    ds_year = set_unwanted_to_nan(ds_year)
 
     # Store data types after modification
-    data_types_after = {var: ds[var].dtype for var in ds.data_vars}
+    data_types_after = {var: ds_year[var].dtype for var in ds_year.data_vars}
     print("Data types after set_unwanted_to_nan:")
     for var, dtype in data_types_after.items():
         print(f'{var}: {dtype}')
 
     # Compare data types before and after
     print("\nData type changes:")
-    for var in ds.data_vars:
+    for var in ds_year.data_vars:
         if data_types_before[var] != data_types_after[var]:
             print(f'{var}: {data_types_before[var]} -> {data_types_after[var]}')
 
-    append_to_zarr(ds, os.path.join(zarr_path, '3Dvars'))
+    # Create a daily mask for the period where HW == 1 for the current year
+    overlapping_mask_year = (
+                ds_daily_grid_hw_year['HW'].sel(time=slice(ds_year['time'].min(), ds_year['time'].max())) == 1)
 
+    # Expand the daily mask to hourly using forward fill for the current year
+    full_hourly_range_year = pd.date_range(start=ds_year['time'].min().values, end=ds_year['time'].max().values,
+                                           freq='H')
+    hourly_mask_year = overlapping_mask_year.reindex(time=full_hourly_range_year, method='ffill').compute()
 
+    # Apply the hourly mask to ds_year to get filtered data for the current year
+    ds_year['HW'] = hourly_mask_year
 
-
-# def process_year(netcdf_dir, zarr_path, log_file_path, year):
-#     for month in range(1, 13):
-#         process_month(netcdf_dir, zarr_path, log_file_path, year, month)
+    append_to_zarr(ds_year, os.path.join(zarr_path, '3Dvars'))
 
 
 if __name__ == "__main__":
-    netcdf_dir = '/home/jguo/process_data/i.e215.I2000Clm50SpGs.hw_production.02/hourly_raw'
-    zarr_path = '/home/jguo/process_data/zarr/test02'
+    # netcdf_dir = '/home/jguo/process_data/i.e215.I2000Clm50SpGs.hw_production.02/hourly_raw'
+    netcdf_dir = '/Trex/case_results/i.e215.I2000Clm50SpGs.hw_production.02/sim_results/hourly'
+    zarr_path = '/Trex/case_results/i.e215.I2000Clm50SpGs.hw_production.02/research_results/zarr'
     output_dir = zarr_path
     os.makedirs(output_dir, exist_ok=True)
     log_file_path = os.path.join(output_dir, 'processed_files.log')
@@ -121,8 +133,15 @@ if __name__ == "__main__":
     ds_hourly = xr.open_dataset(one_hourly_file)
     vars = [var for var in ds_hourly.data_vars if (len(ds_hourly[var].dims) == 3)]
 
-    start_year = 1988
-    end_year = 1999
+    start_year = 1985
+    end_year = 2014
+
+
+    # Read in the daily grid hw netcdf file
+    ds_daily_grid_hw = xr.open_dataset('/home/jguo/process_data/i.e215.I2000Clm50SpGs.hw_production.02/summary/daily_grid_hw.nc')
+
+    ds_daily_grid_hw = convert_time(ds_daily_grid_hw)
+
 
     for year in range(start_year, end_year + 1):
-        process_year(netcdf_dir, zarr_path, log_file_path, year, vars)
+        process_year(netcdf_dir, zarr_path, log_file_path, year, vars, ds_daily_grid_hw)
