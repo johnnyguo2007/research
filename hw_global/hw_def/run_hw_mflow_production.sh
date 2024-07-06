fb77b50fd4d785887324993748e7bb93dcc4bb9f
Johnny Guo <johnnyguo2007@gmail.com>
2024-07-06 10:45:45 -0400

    feature selection

--- hw_global/hw_def/run_hw_mflow_production.sh
+++ hw_global/hw_def/run_hw_mflow_production.sh
@@ -7,7 +7,7 @@ HW_COUNT_THRESHOLD=60
 ITERATIONS=100000
 LEARNING_RATE=0.01
 DEPTH=10
-RUN_TYPE="production"
+RUN_TYPE="Feature_Selection"
 
 # Function to run the experiment
 run_experiment() {
@@ -15,7 +15,8 @@ run_experiment() {
     local hw_percentile=$2
     local merged_file=$3
 
-    python /home/jguo/research/hw_global/final/mlflow_production_run.py \
+    # python /home/jguo/research/hw_global/final/mlflow_production_run.py \
+    python /home/jguo/research/hw_global/final/mlflow_feature_selection.py \
         --summary_dir /Trex/case_results/i.e215.I2000Clm50SpGs.hw_production.02/research_results/hw95_summary \
         --merged_feather_file $merged_file \
         --time_period $time_period \
@@ -23,8 +24,11 @@ run_experiment() {
         --learning_rate $LEARNING_RATE \
         --depth $DEPTH \
         --run_type $RUN_TYPE \
-        --exp_name_extra "HW${hw_percentile}_no_filter" \
-        --shap_calculation
+        --exp_name_extra "Year_Lon_Lat_Direct_and_Delta_HW${hw_percentile}_no_filter" \
+        --shap_calculation \
+        --feature_column "X_Direct" \
+        --delta_mode "include" 
+
 }
 
 # Run experiments for HW95 and HW90, for day and night
@@ -38,3 +42,7 @@ for hw_percentile in 95 90; do
 done
 
 echo "All experiments completed."
+
+# parser.add_argument("--feature_column", type=str, default="X_vars2", help="Column name in df_daily_vars to select features")
+# parser.add_argument("--delta_mode", choices=["none", "include", "only"], default="include", 
+#                     help="'none': don't use delta variables, 'include': use both original and delta variables, 'only': use only delta variables")
