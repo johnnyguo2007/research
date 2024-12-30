3716e65bf07f6b23e168521a5ff1b957e6f0e08f
Johnny Guo <johnnyguo2007@gmail.com>
2024-12-29 23:46:51 -0500

    summary simplified

--- hw_global/ultimate/mlflow_tools/plot_summary.py
+++ hw_global/ultimate/mlflow_tools/plot_summary.py
@@ -22,51 +22,6 @@ def _create_output_dir(output_dir: str, kg_class: str = None) -> str:
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
 
-def _plot_summary(
-    shap_values: np.ndarray,
-    feature_values: np.ndarray,
-    feature_names: List[str],
-    output_dir: str,
-    plot_title: str,
-    file_name: str,
-) -> None:
-    """
-    Generates a SHAP summary plot.
-
-    Args:
-        shap_values (np.ndarray): Array containing SHAP values.
-        feature_values (np.ndarray): Array containing feature values.
-        feature_names (List[str]): List of feature names.
-        output_dir (str): Directory to save the plot.
-        plot_title (str): Title of the plot.
-        file_name (str): Name of the output file.
-    """
-    if shap_values.size == 0 or feature_values.size == 0:
-        logging.warning(f"No data available for {plot_title}. Skipping plot.")
-        return
-
-    try:
-        plt.figure(figsize=(12, 8))
-        shap.summary_plot(
-            shap_values,
-            feature_values,
-            feature_names=feature_names,
-            show=False,
-            plot_size=(12, 8),
-        )
-        plt.title(plot_title)
-        plt.tight_layout()
-        plt.savefig(
-            os.path.join(output_dir, file_name),
-            bbox_inches="tight",
-            dpi=300,
-        )
-        plt.close()
-        logging.info(f"Generated {plot_title}")
-    except Exception as e:
-        logging.error(f"Failed to generate {plot_title}: {str(e)}")
-        plt.close()
-
 def plot_summary(
     shap_values_df: pd.DataFrame,
     feature_values_df: pd.DataFrame,
@@ -82,14 +37,19 @@ def plot_summary(
         f"Starting plot_{plot_type}_summary for {kg_class} with shap_values shape: {shap_values_df.shape} and feature_values shape: {feature_values_df.shape}"
     )
     output_dir = _create_output_dir(output_dir)
-    _plot_summary(
+    plt.figure()
+    shap.summary_plot(
         shap_values_df.values,
         feature_values_df.values,
-        feature_names,
-        output_dir,
-        f"{plot_type.capitalize()} Summary Plot - {kg_class}",
-        f"{plot_type}_summary_plot_{kg_class}.png",
+        feature_names=feature_names,
+        show=False,
+        plot_size=(20, 15),
+        color_bar=True,
     )
+    plt.title(f"{plot_type.capitalize()} Summary Plot - {kg_class}")
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, f"{plot_type}_summary_plot_{kg_class}.png"))
+    plt.close()
 
 def generate_summary_and_kg_plots(
     shap_df: pd.DataFrame,
